3a063f642856b3b3e41a3799f008c5cd53499397
chhong <pnci1029@gmail.com>
2024-01-18 18:53:24 +0900

    fix main test [BackendApplicationTests]

--- src/test/java/com/dmz/BackendApplicationTests.java
+++ src/test/java/com/dmz/BackendApplicationTests.java
@@ -1,14 +1,7 @@
 package com.dmz;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
-
-
-//	@Test
-//	void contextLoads() {
-//	}
-
 }
